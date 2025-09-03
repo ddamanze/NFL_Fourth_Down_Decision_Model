df083dd7de3e19c1969d4d0aec52d94c448ea750
ddamanze <ddamanze@MacBook-Air-6.lan>
2025-09-02 21:28:51 -0400

    creating parquet

--- run_app.py
+++ run_app.py
@@ -37,7 +37,7 @@ def get_process_data():
     return ProcessData()
 
 process_data = get_process_data()
-df = joblib.load('dataset.pkl')
+df = pd.read_parquet('dataset.parquet', engine='pyarrow')
 model_trainer = ModelTrainer(df)
 model_trainer.model()
 pipeline = RunPipeline(model_trainer.df, mode='realtime')
